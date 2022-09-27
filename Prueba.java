058c892fbed32d7035aefce15991c0fb207c04d4
Jorge Rodriguez Rabanal <jrr150502@gmail.com>
2022-09-27 12:03:20 +0200

    aniade edad y anio de nacimiento del usuario

--- Prueba.java
+++ Prueba.java
@@ -3,5 +3,6 @@ public class Prueba {
     public static void main(String args[]) {
 
         System.out.println("Rodriguez Rabanal");
+        System.out.println("20 años, nacido en el año 2002");
     }
 }
