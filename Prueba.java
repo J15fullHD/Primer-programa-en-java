c30c22c939bada96e766be9dcd93f4351013f3ae
Jorge Rodriguez Rabanal <jrr150502@gmail.com>
2022-09-28 10:43:42 +0200

    Apellidos eliminados, grupo favorito aniadido

--- Prueba.java
+++ Prueba.java
@@ -2,8 +2,8 @@ public class Prueba {
 
     public static void main(String args[]) {
 
-        System.out.println("Rodriguez Rabanal");
         System.out.println("20 años, nacido en el año 2002");
         System.out.println("Pizza cuatro quesos");
+        System.out.println("KoRn");
     }
 }
